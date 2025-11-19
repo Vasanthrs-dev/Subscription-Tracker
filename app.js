ece980d7ce7ef6ce551e54c760b166ebcc2d19c0
Vasanth RS <vasanth.rs@proton.me>
2025-11-19 20:28:06 +0530

    2: completed designing model and schema

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRoute from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRoute);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
